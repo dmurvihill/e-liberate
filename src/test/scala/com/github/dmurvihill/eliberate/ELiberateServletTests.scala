be9b22383db97201f51d76e173994886efd171d5
Dolan Murvihill <dolan@Dolans-MacBook-Air.local>
2018-05-25 12:11:26 -0400

    Working acceptance tests
    401 on missing auth
    401 on incorrect auth
    200 on correct auth

--- src/test/scala/com/github/dmurvihill/eliberate/ELiberateServletTests.scala
+++ src/test/scala/com/github/dmurvihill/eliberate/ELiberateServletTests.scala
@@ -1,15 +1,33 @@
 package com.github.dmurvihill.eliberate
 
+import java.util.Base64
 import org.scalatra.test.scalatest._
 
 class ELiberateServletTests extends ScalatraFunSuite {
 
   addServlet(classOf[ELiberateServlet], "/*")
 
-  test("GET / on ELiberateServlet should return status 200") {
+  test("GET / on ELiberateServlet with no authentication should return status 401") {
     get("/") {
-      status should equal (200)
+      status should equal (401)
     }
   }
 
+  test("GET / on ELiberateServlet with valid authentication should return status 401") {
+    get("/", Seq.empty, Seq[(String, String)](basicAuthCredentials("Aladdin", "opencaraway"))) {
+      status should equal (401)
+    }
+  }
+
+  test("GET / on ELiberateServlet with valid authentication should return status 200") {
+    get("/", Seq.empty, Seq[(String, String)](basicAuthCredentials("Aladdin", "opensesame"))) {
+      status should equal(200)
+    }
+  }
+
+  private def basicAuthCredentials(username: String, password: String): (String, String) = {
+    val rawCredentials = username + ":" + password
+    val encCredentials = Base64.getEncoder().encodeToString(rawCredentials)
+    ("Authorization", "Basic " + encCredentials)
+  }
 }
